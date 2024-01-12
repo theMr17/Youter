b204f4884a923378ab1cd7a946e577926fb1acca
Saptak Manna <saptakmanna100@gmail.com>
2024-01-12 15:13:58 +0530

    Implmenent publishAVideo & getVideoById endpoints

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,4 +1,9 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose"
+import { Video } from "../models/video.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
@@ -6,13 +11,89 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
+    const { title, description } = req.body
+    
+    if (!title) {
+        throw new ApiError(400, "Title is required")
+    }
+
+    if (!description) {
+        throw new ApiError(400, "Description is required")
+    }
+
+    if (!Array.isArray(req.files.videoFile)) {
+        throw new ApiError(400, "Video file is required")
+    }
+
+    if (!Array.isArray(req.files.thumbnail)) {
+        throw new ApiError(400, "Thumbnail is required")
+    }
+
+    const videoLocalPath = req.files?.videoFile[0]?.path
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+
+    const video = await uploadOnCloudinary(videoLocalPath)
+
+    if (!video.url) {
+        throw new ApiError(500, "Error while uploading video on cloudinary")
+    }
+
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+    if (!thumbnail.url) {
+        throw new ApiError(500, "Error while uploading thumbnail on cloudinary")
+    }
+
+    const publishedVideo = await Video.create({
+        videoFile: video.url,
+        thumbnail: thumbnail.url,
+        title,
+        description,
+        duration: video.duration,
+        owner: req.user?._id
+    })
+
+    if (!publishedVideo) {
+        throw new ApiError(500, "Error while publishing the video")
+    }
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                201,
+                publishedVideo,
+                "Video published successfully"
+            )
+        )
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
-    // TODO: get video by id
+    
+    if (!videoId) {
+        throw new ApiError(400, "Video id is required")
+    }
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Video id is invalid")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                video,
+                "Video fetched successfully"
+            )
+        )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
