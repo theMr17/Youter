7941b1815fd1deed093f7c138be6c33bb09ce99e
Saptak Manna <saptakmanna100@gmail.com>
2024-02-11 16:35:17 +0530

    Implement toggleTweetLike endpoint

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 import { Like } from "../models/like.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import { Tweet } from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params
@@ -56,7 +57,48 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params
-    // TODO: toggle like on tweet
+    
+    if (!tweetId) {
+        throw new ApiError(400, "Tweet id is required")
+    }
+
+    if (!mongoose.isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Tweet id is invalid")
+    }
+    
+    const like = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user._id),
+                tweet: new mongoose.Types.ObjectId(tweetId)
+            }
+        }
+    ])
+
+    let isLiked, message
+
+    if (!like?.length) {
+        await Like.create({
+            likedBy: req.user?._id,
+            tweet: tweetId
+        })
+        isLiked = true
+        message = "Like added"
+    } else {
+        await Like.findByIdAndDelete(like[0]._id)
+        isLiked = false
+        message = "Like removed"
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { isLiked },
+                message
+            )
+        )
 })
 
 const getLikedVideos = asyncHandler(async (req, res) => {
